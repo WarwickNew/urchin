36834b58b73c6be85fb3b8f84698f63ee0cdcabc
Warwick <warwick.l.e.new@gmail.com>
2023-08-09 16:05:49 +0100

    Fixed errors in client that came in from switching to C99

--- src/client/main.c
+++ src/client/main.c
@@ -2,6 +2,8 @@
 // learn C.
 
 #include "error.h"
+#include "message.h"
+#include <unistd.h>
 #include <netdb.h>      // Contains host structure
 #include <netinet/in.h> //sockaddr_in
 #include <stdio.h>
@@ -56,7 +58,7 @@ int main(int argc, char *argv[]) {
   if (errflag < 0)
     err__crash("Failed to connect to host");
 
-  msg__login_req(sockfd, "username", "passwd");
+  msg__login_req(&sockfd, "username", "passwd");
 
   while (1) {
     bzero(msgbuffer, sizeof msgbuffer);
